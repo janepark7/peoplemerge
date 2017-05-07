2d04c6080f85b0e37e747289a41a859326be0f38
Jane <janeparkjp@gmail.com>
2017-05-07 18:26:07 -0400

    final push, completed people merge app

--- people.js
+++ people.js
@@ -7,5 +7,17 @@ const people2 = require ("./people2.json");
 // console.log(people1);
 // console.log(people2);
 //
-let peopleMerge = people1.concat(people2);
-console.log(peopleMerge.sort());
+let peopleMerge = people1.concat(people2).sort();
+//console.log(peopleMerge.sort());
+let output = '';
+
+for (var i = 0; i < peopleMerge.length; i++) {
+	 output += peopleMerge[i] + "\n";
+}
+
+console.log(output);
+
+fs.writeFile('./sorted_people.txt', output, (err) => {
+  if (err) throw err;
+  console.log("Success!");
+});
